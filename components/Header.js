298a94e9afac462b8486a76052540dfd80364d87
Aditi Singh <aditisinghdeveloper@gmail.com>
2022-02-12 23:05:05 +0530

    AirBnb-clone

---
+++ components/Header.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+function Header() {
+    return (
+        <header>
+        
+        {/*Left*/}
+<div>
+   <Image src="https://links.papareact.com/qd3" layout="fill"/>
+</div>
+
+        {/*Middle*/}
+<div></div>
+
+        {/*Right*/}
+<div></div>
+        </header>
+    )
+}
+
+export default Header
+
